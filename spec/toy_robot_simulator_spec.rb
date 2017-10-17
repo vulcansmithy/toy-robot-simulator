efe2a5848abd111cb7bcf1dac44d6e2e246987e5
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2017-10-18 06:04:39 +0800

    Added rspec test 'should ignore the command when running execute_place_command when given an out of bound x and y position' and made it pass.

--- spec/toy_robot_simulator_spec.rb
+++ spec/toy_robot_simulator_spec.rb
@@ -116,5 +116,45 @@ describe ToyRobotSimulator do
     expect(@toy_robot.point_y  ).to eq 4
     expect(@toy_robot.facing_at).to eq "EAST"
   end
+  
+  it "should ignore the command when running execute_place_command when given an out of bound x and y position" do
+    
+    # make sure point_x, point_y and facing_at are initialize state of nil
+    expect(@toy_robot.point_x  ).to eq nil
+    expect(@toy_robot.point_y  ).to eq nil
+    expect(@toy_robot.facing_at).to eq nil
+    
+    # call execute_place_command and set position to the origin position and facing north
+    result = @toy_robot.execute_place_command(2, 3, "north")
+    
+    expect(result              ).to eq true
+    expect(@toy_robot.point_x  ).to eq 2
+    expect(@toy_robot.point_y  ).to eq 3
+    expect(@toy_robot.facing_at).to eq "NORTH"
+    
+    # call execute_place_command and give it a position that is out of bound
+    result = @toy_robot.execute_place_command(3, 5, "east")
+    
+    expect(result              ).to eq false
+    expect(@toy_robot.point_x  ).to eq 2
+    expect(@toy_robot.point_y  ).to eq 3
+    expect(@toy_robot.facing_at).to eq "NORTH"
+    
+    # call execute_place_command and give it a position that is out of bound
+    result = @toy_robot.execute_place_command(5, 1, "east")
+    
+    expect(result              ).to eq false
+    expect(@toy_robot.point_x  ).to eq 2
+    expect(@toy_robot.point_y  ).to eq 3
+    expect(@toy_robot.facing_at).to eq "NORTH"
+    
+    # call execute_place_command and give it a position that is out of bound
+    result = @toy_robot.execute_place_command(-1, 3, "east")
+    
+    expect(result              ).to eq false
+    expect(@toy_robot.point_x  ).to eq 2
+    expect(@toy_robot.point_y  ).to eq 3
+    expect(@toy_robot.facing_at).to eq "NORTH"
+  end
 
 end
