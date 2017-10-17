493b2a4e08bd8f22e8c081170829098c40c2493e
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2017-10-18 00:49:13 +0800

    Created an rspec test for testing for processing a valid 'place' command.

--- spec/toy_robot_simulator_spec.rb
+++ spec/toy_robot_simulator_spec.rb
@@ -1,5 +1,14 @@
 require "spec_helper"
  
 describe ToyRobotSimulator do
+  
+  before :each do
+    @toy_robot = ToyRobotSimulator.new
+  end  
+  
+  it "be able proccess a valid 'place' command" do
+    command = "place 0,0,north"  
+    expect(@toy_robot.proccess_command(command)).to eq true
+  end
 
 end
