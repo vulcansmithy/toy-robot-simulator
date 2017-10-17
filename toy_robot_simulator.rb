c49dcbff45c8241eeeff7d8de24fb02c14c6db42
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2017-10-18 00:21:11 +0800

    Enable the file toy_robot_simulator.rb to run from the command line by typing 'ruby toy_robot_simulator.rb'.

--- toy_robot_simulator.rb
+++ toy_robot_simulator.rb
@@ -1,3 +1,16 @@
+require File.dirname(__FILE__) + "/toy_robot_simulator.rb"
+
+# this enable this ruby script to be run from the command output. Example - $> ruby toy_robot_simulator.rb
+if __FILE__ == $PROGRAM_NAME
+  ToyRobotSimulator.new.main 
+end
+
 class ToyRobotSimulator
+  
+  def main
+    puts "@DEBUG #{__LINE__}    Running inside main..."
+    
+    return 0
+  end  
   
 end
