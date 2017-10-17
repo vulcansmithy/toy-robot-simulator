f078b782a81efde714bc7fb62bb62ad5e241d336
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2017-10-18 00:13:42 +0800

    Setup necessary files to able to run rspec test. Created 2 rspec test to see if it can successfully run the test. One successful. The other fails.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,1 @@
+require_relative "../toy_robot_simulator"
---
+++ spec/toy_robot_simulator_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+ 
+describe ToyRobotSimulator do
+ 
+  it "should be able to run this rspec test" do
+    expect(true).to eq true
+  end
+  
+  it "should expect to fail" do
+    expect(true).to eq false
+  end
+ 
+end
